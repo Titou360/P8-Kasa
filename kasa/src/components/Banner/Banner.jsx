8fbc1c004d7827844c0e4b3ec1d40fb5877cffdd
Titou360 <clement.felices@gmail.com>
2023-09-05 15:02:48 +0200

    Update the commit beacause 2 folders in Project-2

--- kasa/src/components/Banner/Banner.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Banner({ text, background }) {
-  return (
-    <div className="banner" style={{ backgroundImage: `url(${background})` }}>
-      {text && <h1 className="banner--text">{text}</h1>}
-    </div>
-  );
-}
--- kasa/src/routes/About.jsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-      <div>
-          <h1>🙂</h1>
-      </div>
-  )
-}
