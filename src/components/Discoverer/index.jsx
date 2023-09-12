0ec42aea05cbdceac0d3d4254d4abdab5090ee5f
Titou360 <clement.felices@gmail.com>
2023-09-12 09:25:34 +0200

    animation in discoverer fixed

--- src/components/Discoverer/index.jsx
+++ src/components/Discoverer/index.jsx
@@ -1,39 +1,28 @@
 import { useState } from "react";
 import arrow from "../Discoverer/arrow_back.png";
 
-export default function Discoverer({ title, content, id }) {
+export default function Discoverer({ title, content}) {
   const [isOpen, setIsOpen] = useState(false);
-  const [isFadingOut, setIsFadingOut] = useState(false); // Nouvel état
+
+  const arrowIconClass = isOpen ? "arrow-icon" : "arrow-icon";
+
+  const contentLineUp = `discoverer--content ${isOpen ? "open" : "closed"} ${isOpen ? "lineUp" : ""}`;
+
+ 
 
   const toggleDiscoverer = () => {
-    if (isOpen) {
-      setIsFadingOut(true); // Active the fade effect
-      setTimeout(() => {
-        setIsOpen(false); // Close the Discoverer after the fade effect
-      }, 500); // Wait 500 ms (0.5s) for the fade effect finishing
-    } else {
-      setIsOpen(true);
-      setIsFadingOut(false); // Desactivate the fade effect
-    }
-  };
-
-  const arrowStyle = {
-    transform: isOpen ? "rotate(-90deg)" : "rotate(90deg)",
-    transition: "transform 0.5s ease",
-  };
-
-  const contentStyle = {
-    opacity: isFadingOut ? 0 : 1, // Utilisez l'état isFadingOut pour gérer l'opacité
-    transition: "opacity 0.5s ease",
+    setIsOpen(!isOpen);
   };
 
   return (
     <div className={`discoverer ${isOpen ? "open" : "closed"}`}>
+
       <div className="discoverer--header" onClick={toggleDiscoverer}>
         <h3>{title}</h3>
-        <img src={arrow} alt="Icône" style={arrowStyle} className="arrow-icon" />
+        <img src={arrow} alt="Icône" className={arrowIconClass} />
       </div>
-      <div className="discoverer--content" style={contentStyle}>
+      <div className={contentLineUp}>
+
         {title === "Équipements" ? (
           <div className="property--equipments">
             {content.map((equipment, index) => (
